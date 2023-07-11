b9fcf351011623106057fc5ec767d6f1f8e65046
Divyansh Kushwaha <divyanshdxn@gmail.com>
2023-07-11 22:35:03 +0530

    formatted

--- generate_android.py
+++ generate_android.py
@@ -64,7 +64,7 @@ with open(csv_file, 'r') as file:
 
             # Write the XML tree to the file
             tree = ET.ElementTree(root)
-            ET.indent(tree, "    ")
+            ET.indent(tree, "  ") # two space tab in 
             tree.write(xml_file, encoding='utf-8', xml_declaration=True)
 
 print("XML files android_res successfully!")
